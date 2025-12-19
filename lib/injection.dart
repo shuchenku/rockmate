22eff7dbab0c05ceb3417abf52343b2160f461ac
Shu Chen <chenshu8805@gmail.com>
2025-12-18 20:47:03 -0500

    fix: manually register OpenBetaClient in DI container

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:openbeta_client/openbeta_client.dart';
 import 'injection.config.dart';
 
 final getIt = GetIt.instance;
@@ -9,4 +10,8 @@ final getIt = GetIt.instance;
   preferRelativeImports: true, // default
   asExtension: true, // default
 )
-void configureDependencies() => getIt.init();
+void configureDependencies() {
+  getIt.init();
+  // Manually register OpenBetaClient since it's from external package
+  getIt.registerFactory<OpenBetaClient>(() => OpenBetaClient());
+}

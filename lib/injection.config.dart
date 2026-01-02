4fb5c26f0a709064e0602be951309681ada9062a
Shu Chen <chenshu8805@gmail.com>
2026-01-01 21:40:10 -0500

    tech-debt(issue-42): remove unused RouteRepository and RouteLocalDataSource
    - RouteSearchBloc already migrated to ClimbRepository
    - Removed unused RouteRepository class
    - Removed unused RouteLocalDataSource class
    - Regenerated injection.config.dart to remove old registrations
    - Both search and detail blocs now consistent with ClimbRepository
    
    Closes #42

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -12,10 +12,6 @@
 import 'package:climb_data/climb_data.dart' as _i597;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
-import 'package:rockmate/features/climbing_data/data/datasources/route_local_data_source.dart'
-    as _i504;
-import 'package:rockmate/features/climbing_data/data/repositories/route_repository.dart'
-    as _i497;
 import 'package:rockmate/features/climbing_data/presentation/bloc/route_detail_bloc.dart'
     as _i412;
 import 'package:rockmate/features/climbing_data/presentation/bloc/route_search_bloc.dart'
@@ -28,15 +24,11 @@ extension GetItInjectableX on _i174.GetIt {
     _i526.EnvironmentFilter? environmentFilter,
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
-    gh.factory<_i504.RouteLocalDataSource>(() => _i504.RouteLocalDataSource());
-    gh.factory<_i497.RouteRepository>(
-      () => _i497.RouteRepository(gh<_i504.RouteLocalDataSource>()),
+    gh.factory<_i71.RouteSearchBloc>(
+      () => _i71.RouteSearchBloc(gh<_i597.ClimbRepository>()),
     );
     gh.factory<_i412.RouteDetailBloc>(
       () => _i412.RouteDetailBloc(gh<_i597.ClimbRepository>()),
-    );
-    gh.factory<_i71.RouteSearchBloc>(
-      () => _i71.RouteSearchBloc(gh<_i597.ClimbRepository>()),
     );
     return this;
   }
